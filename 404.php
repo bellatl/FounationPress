1b3325923abcc0de3fb291294f001fdfc9601651
bellatl <bellatl@users.noreply.github.com>
2017-09-12 10:41:33 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -8,16 +8,17 @@
 
 get_header(); ?>
 
-<div class="row">
-	<div class="small-12 large-8 columns" role="main">
+<div class="row pageWrapper animated fadeIn">
+	<div class="small-12 large-8 columns" role="main"><br />
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php _e( 'File Not Found', 'foundationpress' ); ?></h1>
+				<h1 class="entry-title"><?php _e( 'Page Not Found', 'foundationpress' ); ?></h1>
 			</header>
+			<hr />
 			<div class="entry-content">
 				<div class="error">
-					<p class="bottom"><?php _e( 'The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'foundationpress' ); ?></p>
+					<p class="bottom"><?php _e( 'The page you are looking for might have been removed or is temporarily unavailable.', 'foundationpress' ); ?></p>
 				</div>
 				<p><?php _e( 'Please try the following:', 'foundationpress' ); ?></p>
 				<ul>
@@ -36,7 +37,7 @@ get_header(); ?>
 			</div>
 		</article>
 
-	</div>
+	</div><br />
 	<?php get_sidebar(); ?>
 </div>
 <?php get_footer();
