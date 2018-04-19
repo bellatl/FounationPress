c9e7b3e747ceac74fe0da985c3299d65683913a2
bellatl <bellatl@users.noreply.github.com>
2018-04-19 14:29:39 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="footer-container container footer hide-for-print" data-sticky-footer>
 			<footer class="footer">
 				<?php do_action( 'foundationpress_before_footer' ); ?>
-					<p class="show-for-medium">Copyright&copy; 2017 Northside Hospital Inc. All Rights Reserved.</p><p class="show-for-small-only">Copyright&copy; 2017 Northside Hospital Inc.<br />A Northside Hospital Physician Practice.<br />All Rights Reserved.<br /><a href="http://www.northside.com/NSH-Privacy-Policy" target="_blank"><u>Privacy Policy</u></a></p><br />
+					<p class="show-for-medium">Copyright&copy; <?php echo date("Y"); ?> Northside Hospital Inc. All Rights Reserved.</p><p class="show-for-small-only">Copyright&copy; <?php echo date("Y"); ?> Northside Hospital Inc.<br />A Northside Hospital Physician Practice.<br />All Rights Reserved.<br /><a href="http://www.northside.com/NSH-Privacy-Policy" target="_blank"><u>Privacy Policy</u></a></p><br />
 		      <div class="hide-for-small-only"><a href="http://browsehappy.com/" target="_blank" title="Update your Browser"><p><strong>Having trouble viewing this site?</strong><br />This site is compatible with Chrome, Firefox, Safari, Internet Explorer 9,10,11, &amp; Microsoft Edge</a><br /><a href="http://www.northside.com/NSH-Privacy-Policy" target="_blank"><u>Privacy Policy</u></a></p></div>
 				<?php do_action( 'foundationpress_after_footer' ); ?>
 			</footer>
